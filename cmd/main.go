77ec14855fd9a92e6da99cc81e18b6b46ee510f5
Akshit Agrawal <me@akshitagrawal.dev>
2024-01-16 11:50:37 +0530

    few fixes

--- cmd/main.go
+++ cmd/main.go
@@ -18,7 +18,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	federalToken, err := gcp.GetFederalToken(oidcIdToken, projectId, poolId, projectId)
+	federalToken, err := gcp.GetFederalToken(oidcIdToken, projectId, poolId, providerId)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
