dfa044ab9a61a03233ba65567a27f85d038c26c6
Akshit Agrawal <me@akshitagrawal.dev>
2024-01-17 11:05:02 +0530

    env vars changed

--- cmd/main.go
+++ cmd/main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main() {
-	oidcIdToken := os.Getenv("PLUGIN_OIDC_ID_TOKEN")
-	projectId := os.Getenv("PLUGIN_PROJECT_NUMBER")
+	oidcIdToken := os.Getenv("PLUGIN_OIDC_TOKEN_ID")
+	projectId := os.Getenv("PLUGIN_PROJECT_ID")
 	poolId := os.Getenv("PLUGIN_POOL_ID")
 	providerId := os.Getenv("PLUGIN_PROVIDER_ID")
-	serviceAccountEmail := os.Getenv("PLUGIN_SERVICE_ACCOUNT_EMAIL")
+	serviceAccountEmail := os.Getenv("PLUGIN_SERVICE_ACCOUNT_EMAIL_ID")
 
 	if oidcIdToken == "" || projectId == "" || poolId == "" || providerId == "" || serviceAccountEmail == "" {
 		fmt.Println("Missing required environment variables")
